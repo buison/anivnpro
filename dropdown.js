a4d7c1662e03c98b76cdaeb94b2de0217f921191
buison <buison@users.noreply.github.com>
2017-04-21 11:32:55 +0700

    Add files via upload

---
+++ dropdown.js
@@ -0,0 +1,404 @@
+﻿/* jQuery menu */
+$(document).ready(function () {
+    $("#top li").hover(function () {
+        $(this).find('ul:first').css({
+            visibility: "visible",
+            display: "none"
+        }).show(400);
+    }, function () {
+        $(this).find('ul:first').css({
+            visibility: "hidden"
+        });
+    });
+});
+
+/* ẩn hiện nội dung phim */
+jQuery(document).ready(function ($) {
+	var a = $('#description-clip'),mhv = 150;
+	if (a.height() > mhv) {
+		a.height(mhv).after('<div class="toggle-des"><span id="decs-ex" data-active="0" onclick="MARX.Utilities.Exdesc(this)">Hiển thị đầy đủ</span></div>')
+	}
+});
+
+/* đóng góp ý kiến website */
+$(function () {
+    $('body').append('<div id="screen"></div>')
+    $('body').append(
+        '<div id="box_sendmail" class="box-alert">' +
+        '<div>' +
+        '<h3>Yêu cầu anime cho website !</h3>' +
+        '<form method="post" action="http://www.anivn.com/yeu-cau/mail.php">' +
+        '<p>Tên bạn :<input type="text" tabindex="1" placeholder="Tên của bạn" id="input-gopy" name="fullname"/></p>' +
+        '<p>Email : <input type="text" tabindex="1" placeholder="Địa chỉ email bạn" id="input-gopy" name="email"/></p>' +
+        '<p>Tin nhắn : <br><textarea placeholder="Nhập tên bộ anime bạn cần yêu cầu..." id="input-gopy" style="width:96%; padding:5" tabindex="1" name="comments" cols="40" rows="6"></textarea></p>' +
+        '<input type="submit" value="Gửi ngay" id="submit-gopy"/><input type="reset" id="reset-gopy" tabindex="1" value="Nhập lại">' +
+        '</form>' +
+        '<p>Yêu cầu của bạn sẽ giúp chúng tôi phục vụ được tốt hơn !</p>' +
+        '</div>' +
+        '<div class="box-alert-close"></div>' +
+        '</div>' +
+        '<div id="but-gopy"></div>'
+    )
+
+    function sendmail() {
+        center()
+        $('#screen').show()
+        $('#box_sendmail').fadeIn('fast')
+        return false
+    }
+    //close alert
+    $('.box-alert-close').live('click', function () {
+        $('#screen,.box-alert').hide()
+    })
+    //function center
+    center();
+    $(window).resize(function () {
+        center()
+    })
+
+    function center() {
+        var $wid = $(window).width()
+        var $hei = $(window).height()
+        $('.box-alert').each(function () {
+            $(this).css({
+                'left': ($wid - $(this).width()) / 2,
+                'top': ($hei - $(this).height()) / 2
+            })
+        })
+    }
+    $('#but-gopy,a[href^=sendmessage]').click(function () {
+        sendmail()
+        return false
+    })
+})
+/* lên đầu trang */
+$(function () {
+    $.fn.scrollToTop = function () {
+        $(this).hide().removeAttr("href");
+        if ($(window).scrollTop() != "0") {
+            $(this).fadeIn("slow")
+        }
+        var scrollDiv = $(this);
+        $(window).scroll(function () {
+            if ($(window).scrollTop() == "0") {
+                $(scrollDiv).fadeOut("slow")
+            } else {
+                $(scrollDiv).fadeIn("slow")
+            }
+        });
+        $(this).click(function () {
+            $("html, body").animate({
+                scrollTop: 0
+            }, "slow")
+        })
+    }
+});
+$(function () {
+    $("#lendautrang").scrollToTop();
+});
+
+/* Lazy Load 1.9.3 */
+!function(a,b,c,d){var e=a(b);a.fn.lazyload=function(f){function g(){var b=0;i.each(function(){var c=a(this);if(!j.skip_invisible||c.is(":visible"))if(a.abovethetop(this,j)||a.leftofbegin(this,j));else if(a.belowthefold(this,j)||a.rightoffold(this,j)){if(++b>j.failure_limit)return!1}else c.trigger("appear"),b=0})}var h,i=this,j={threshold:0,failure_limit:0,event:"scroll",effect:"show",container:b,data_attribute:"original",skip_invisible:!0,appear:null,load:null,placeholder:"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"};return f&&(d!==f.failurelimit&&(f.failure_limit=f.failurelimit,delete f.failurelimit),d!==f.effectspeed&&(f.effect_speed=f.effectspeed,delete f.effectspeed),a.extend(j,f)),h=j.container===d||j.container===b?e:a(j.container),0===j.event.indexOf("scroll")&&h.bind(j.event,function(){return g()}),this.each(function(){var b=this,c=a(b);b.loaded=!1,(c.attr("src")===d||c.attr("src")===!1)&&c.is("img")&&c.attr("src",j.placeholder),c.one("appear",function(){if(!this.loaded){if(j.appear){var d=i.length;j.appear.call(b,d,j)}a("<img />").bind("load",function(){var d=c.attr("data-"+j.data_attribute);c.hide(),c.is("img")?c.attr("src",d):c.css("background-image","url('"+d+"')"),c[j.effect](j.effect_speed),b.loaded=!0;var e=a.grep(i,function(a){return!a.loaded});if(i=a(e),j.load){var f=i.length;j.load.call(b,f,j)}}).attr("src",c.attr("data-"+j.data_attribute))}}),0!==j.event.indexOf("scroll")&&c.bind(j.event,function(){b.loaded||c.trigger("appear")})}),e.bind("resize",function(){g()}),/(?:iphone|ipod|ipad).*os 5/gi.test(navigator.appVersion)&&e.bind("pageshow",function(b){b.originalEvent&&b.originalEvent.persisted&&i.each(function(){a(this).trigger("appear")})}),a(c).ready(function(){g()}),this},a.belowthefold=function(c,f){var g;return g=f.container===d||f.container===b?(b.innerHeight?b.innerHeight:e.height())+e.scrollTop():a(f.container).offset().top+a(f.container).height(),g<=a(c).offset().top-f.threshold},a.rightoffold=function(c,f){var g;return g=f.container===d||f.container===b?e.width()+e.scrollLeft():a(f.container).offset().left+a(f.container).width(),g<=a(c).offset().left-f.threshold},a.abovethetop=function(c,f){var g;return g=f.container===d||f.container===b?e.scrollTop():a(f.container).offset().top,g>=a(c).offset().top+f.threshold+a(c).height()},a.leftofbegin=function(c,f){var g;return g=f.container===d||f.container===b?e.scrollLeft():a(f.container).offset().left,g>=a(c).offset().left+f.threshold+a(c).width()},a.inviewport=function(b,c){return!(a.rightoffold(b,c)||a.leftofbegin(b,c)||a.belowthefold(b,c)||a.abovethetop(b,c))},a.extend(a.expr[":"],{"below-the-fold":function(b){return a.belowthefold(b,{threshold:0})},"above-the-top":function(b){return!a.belowthefold(b,{threshold:0})},"right-of-screen":function(b){return a.rightoffold(b,{threshold:0})},"left-of-screen":function(b){return!a.rightoffold(b,{threshold:0})},"in-viewport":function(b){return a.inviewport(b,{threshold:0})},"above-the-fold":function(b){return!a.belowthefold(b,{threshold:0})},"right-of-fold":function(b){return a.rightoffold(b,{threshold:0})},"left-of-fold":function(b){return!a.rightoffold(b,{threshold:0})}})}(jQuery,window,document);
+
+/*******************************
+* Hiệu ứng trượt site mượt
+* Tác giả : Võ Phước Tiến
+* Yahoo : Tien.Design
+* Email : Admin@AniVN.Com
+********************************/
+function ssc_init() {
+    if (!document.body) return;
+    var e = document.body;
+    var t = document.documentElement;
+    var n = window.innerHeight;
+    var r = e.scrollHeight;
+    ssc_root = document.compatMode.indexOf("CSS") >= 0 ? t : e;
+    ssc_activeElement = e;
+    ssc_initdone = true;
+    if (top != self) {
+        ssc_frame = true
+    } else if (r > n && (e.offsetHeight <= n || t.offsetHeight <= n)) {
+        ssc_root.style.height = "auto";
+        if (ssc_root.offsetHeight <= n) {
+            var i = document.createElement("div");
+            i.style.clear = "both";
+            e.appendChild(i)
+        }
+    }
+    if (!ssc_fixedback) {
+        e.style.backgroundAttachment = "scroll";
+        t.style.backgroundAttachment = "scroll"
+    }
+    if (ssc_keyboardsupport) {
+        ssc_addEvent("keydown", ssc_keydown)
+    }
+}
+
+function ssc_scrollArray(e, t, n, r) {
+    r || (r = 1e3);
+    ssc_directionCheck(t, n);
+    ssc_que.push({
+        x: t,
+        y: n,
+        lastX: t < 0 ? .99 : -.99,
+        lastY: n < 0 ? .99 : -.99,
+        start: +(new Date)
+    });
+    if (ssc_pending) {
+        return
+    }
+    var i = function () {
+        var s = +(new Date);
+        var o = 0;
+        var u = 0;
+        for (var a = 0; a < ssc_que.length; a++) {
+            var f = ssc_que[a];
+            var l = s - f.start;
+            var c = l >= ssc_animtime;
+            var h = c ? 1 : l / ssc_animtime;
+            if (ssc_pulseAlgorithm) {
+                h = ssc_pulse(h)
+            }
+            var p = f.x * h - f.lastX >> 0;
+            var d = f.y * h - f.lastY >> 0;
+            o += p;
+            u += d;
+            f.lastX += p;
+            f.lastY += d;
+            if (c) {
+                ssc_que.splice(a, 1);
+                a--
+            }
+        }
+        if (t) {
+            var v = e.scrollLeft;
+            e.scrollLeft += o;
+            if (o && e.scrollLeft === v) {
+                t = 0
+            }
+        }
+        if (n) {
+            var m = e.scrollTop;
+            e.scrollTop += u;
+            if (u && e.scrollTop === m) {
+                n = 0
+            }
+        }
+        if (!t && !n) {
+            ssc_que = []
+        }
+        if (ssc_que.length) {
+            setTimeout(i, r / ssc_framerate + 1)
+        } else {
+            ssc_pending = false
+        }
+    };
+    setTimeout(i, 0);
+    ssc_pending = true
+}
+
+function ssc_wheel(e) {
+    if (!ssc_initdone) {
+        ssc_init()
+    }
+    var t = e.target;
+    var n = ssc_overflowingAncestor(t);
+    if (!n || e.defaultPrevented || ssc_isNodeName(ssc_activeElement, "embed") || ssc_isNodeName(t, "embed") && /\.pdf/i.test(t.src)) {
+        return true
+    }
+    var r = e.wheelDeltaX || 0;
+    var i = e.wheelDeltaY || 0;
+    if (!r && !i) {
+        i = e.wheelDelta || 0
+    }
+    if (Math.abs(r) > 1.2) {
+        r *= ssc_stepsize / 120
+    }
+    if (Math.abs(i) > 1.2) {
+        i *= ssc_stepsize / 120
+    }
+    ssc_scrollArray(n, -r, -i);
+    e.preventDefault()
+}
+
+function ssc_keydown(e) {
+    var t = e.target;
+    var n = e.ctrlKey || e.altKey || e.metaKey;
+    if (/input|textarea|embed/i.test(t.nodeName) || t.isContentEditable || e.defaultPrevented || n) {
+        return true
+    }
+    if (ssc_isNodeName(t, "button") && e.keyCode === ssc_key.spacebar) {
+        return true
+    }
+    var r, i = 0,
+        s = 0;
+    var o = ssc_overflowingAncestor(ssc_activeElement);
+    var u = o.clientHeight;
+    if (o == document.body) {
+        u = window.innerHeight
+    }
+    switch (e.keyCode) {
+    case ssc_key.up:
+        s = -ssc_arrowscroll;
+        break;
+    case ssc_key.down:
+        s = ssc_arrowscroll;
+        break;
+    case ssc_key.spacebar:
+        r = e.shiftKey ? 1 : -1;
+        s = -r * u * .9;
+        break;
+    case ssc_key.pageup:
+        s = -u * .9;
+        break;
+    case ssc_key.pagedown:
+        s = u * .9;
+        break;
+    case ssc_key.home:
+        s = -o.scrollTop;
+        break;
+    case ssc_key.end:
+        var a = o.scrollHeight - o.scrollTop - u;
+        s = a > 0 ? a + 10 : 0;
+        break;
+    case ssc_key.left:
+        i = -ssc_arrowscroll;
+        break;
+    case ssc_key.right:
+        i = ssc_arrowscroll;
+        break;
+    default:
+        return true
+    }
+    ssc_scrollArray(o, i, s);
+    e.preventDefault()
+}
+
+function ssc_mousedown(e) {
+    ssc_activeElement = e.target
+}
+
+function ssc_setCache(e, t) {
+    for (var n = e.length; n--;) ssc_cache[ssc_uniqueID(e[n])] = t;
+    return t
+}
+
+function ssc_overflowingAncestor(e) {
+    var t = [];
+    var n = ssc_root.scrollHeight;
+    do {
+        var r = ssc_cache[ssc_uniqueID(e)];
+        if (r) {
+            return ssc_setCache(t, r)
+        }
+        t.push(e);
+        if (n === e.scrollHeight) {
+            if (!ssc_frame || ssc_root.clientHeight + 10 < n) {
+                return ssc_setCache(t, document.body)
+            }
+        } else if (e.clientHeight + 10 < e.scrollHeight) {
+            overflow = getComputedStyle(e, "").getPropertyValue("overflow");
+            if (overflow === "scroll" || overflow === "auto") {
+                return ssc_setCache(t, e)
+            }
+        }
+    } while (e = e.parentNode)
+}
+
+function ssc_addEvent(e, t, n) {
+    window.addEventListener(e, t, n || false)
+}
+
+function ssc_removeEvent(e, t, n) {
+    window.removeEventListener(e, t, n || false)
+}
+
+function ssc_isNodeName(e, t) {
+    return e.nodeName.toLowerCase() === t.toLowerCase()
+}
+
+function ssc_directionCheck(e, t) {
+    e = e > 0 ? 1 : -1;
+    t = t > 0 ? 1 : -1;
+    if (ssc_direction.x !== e || ssc_direction.y !== t) {
+        ssc_direction.x = e;
+        ssc_direction.y = t;
+        ssc_que = []
+    }
+}
+
+function ssc_pulse_(e) {
+    var t, n, r;
+    e = e * ssc_pulseScale;
+    if (e < 1) {
+        t = e - (1 - Math.exp(-e))
+    } else {
+        n = Math.exp(-1);
+        e -= 1;
+        r = 1 - Math.exp(-e);
+        t = n + r * (1 - n)
+    }
+    return t * ssc_pulseNormalize
+}
+
+function ssc_pulse(e) {
+    if (e >= 1) return 1;
+    if (e <= 0) return 0;
+    if (ssc_pulseNormalize == 1) {
+        ssc_pulseNormalize /= ssc_pulse_(1)
+    }
+    return ssc_pulse_(e)
+}
+
+var ssc_framerate = 150;
+var ssc_animtime = 500;
+var ssc_stepsize = 150;
+var ssc_pulseAlgorithm = true;
+var ssc_pulseScale = 6;
+var ssc_pulseNormalize = 1;
+var ssc_keyboardsupport = true;
+var ssc_arrowscroll = 50;
+var ssc_frame = false;
+var ssc_direction = {
+    x: 0,
+    y: 0
+};
+
+var ssc_initdone = false;
+var ssc_fixedback = true;
+var ssc_root = document.documentElement;
+var ssc_activeElement;
+var ssc_key = {
+    left: 37,
+    up: 38,
+    right: 39,
+    down: 40,
+    spacebar: 32,
+    pageup: 33,
+    pagedown: 34,
+    end: 35,
+    home: 36
+};
+
+var ssc_que = [];
+var ssc_pending = false;
+var ssc_cache = {};
+
+setInterval(function () {
+    ssc_cache = {}
+}, 10 * 1e3);
+
+var ssc_uniqueID = function () {
+    var e = 0;
+    return function (t) {
+        return t.ssc_uniqueID || (t.ssc_uniqueID = e++)
+    }
+}();
+
+var ischrome = /chrome/.test(navigator.userAgent.toLowerCase());
+
+if (ischrome) {
+    ssc_addEvent("mousedown", ssc_mousedown);
+    ssc_addEvent("mousewheel", ssc_wheel);
+    ssc_addEvent("load", ssc_init)
+}
